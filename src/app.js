9e1ed9872787201cfbde9c8318f9af8b54e75d63
Jesse Tsang <1919892+JesseTsang@users.noreply.github.com>
2020-08-12 20:37:11 -0400

    Started working on main pages route details

--- src/app.js
+++ src/app.js
@@ -7,14 +7,30 @@ const http = require('http');
 const url = require('url');
 const path = require('path');
 
-// Top-level code: Execute only once
+// Top-level code: Execute only once (So synchronious code is okay)
 
-// Read JSON Data File
-const getFilePath = process.cwd() + '/dev-data/data.json';
-const data = fs.readFileSync(getFilePath, 'utf-8', (err, data) => {
+// 1. Read JSON Data File
+const getProductJSONPath = process.cwd() + '/dev-data/data.json';
+const data = fs.readFileSync(getProductJSONPath, 'utf-8', (err, data) => {
     if(err) return console.log('ERROR! 💢💢💢 Product JSON file not found!');  
 });
 const productDataObject = JSON.parse(data);
+
+// 2. Read static templates
+const getTemplateOverviewPath = `${__dirname}/template-overview.html`;
+const templateOverview = fs.readFileSync(getTemplateOverviewPath, 'utf-8', (err, data) => {
+    if(err) return console.log('ERROR! 💢💢💢 template-overview.html file not found!');  
+});
+
+const getTemplateProdPath = `${__dirname}/template-product.html`;
+const templateProduct = fs.readFileSync(getTemplateProdPath, 'utf-8', (err, data) => {
+    if(err) return console.log('ERROR! 💢💢💢 template-product.html JSON file not found!');  
+});
+
+const getTemplateCardPath = `${__dirname}/template-card.html`;
+const templateCard = fs.readFileSync(getTemplateCardPath, 'utf-8', (err, data) => {
+    if(err) return console.log('ERROR! 💢💢💢 template-card.html JSON file not found!');  
+});
 
 // END TOP-LEVEL CODE
 
@@ -22,25 +38,31 @@ const productDataObject = JSON.parse(data);
 const server = http.createServer((req, res) => {
     const pathName = req.url;
     
-    // Routing conditions
+    // Routing conditions - START
+    
+    // 1. Overview Page Route
     if(pathName === '/' || pathName === '/overview') {
-        res.end('This is the OVERVIEW page!');
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.end(templateOverview);
     }
     else if (pathName === '/product') {
+        // 2. Product Page Route
         res.end('This is the PRODUCT page!');
     }
     else if (pathName === '/api') {  
-        // Note: You can only send back String data, not object
-        // Note: so only 'data' and not 'productData'.
+        // 3. API Route
         res.writeHead(200, {'Content-type': 'application/json'});
         res.end(data);                            
     }
     else {
+        // 4. 404 Page Route
         res.writeHead(404, {
             'Content-type': 'text/html'
         });
         res.end('<h1>Page not found!</h1>'); 
     }
+
+    // Routing conditions - END
 });
 
 // LISTENER SETUP
